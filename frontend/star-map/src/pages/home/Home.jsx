f2f3ca6b394764a2c657d950e5c3b23d0d8fadbd
yosef-w <ywolday24@gmail.com>
2023-05-21 19:13:36 -0500

    Add components to homepage

--- frontend/star-map/src/pages/home/Home.jsx
+++ frontend/star-map/src/pages/home/Home.jsx
@@ -1,15 +1,41 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
-import "./home.css"
-
+import './home.css';
 
 export default function Home() {
   return (
     <div>
-      <div>
-        <div className="support-square" style={{width: "100%", height: "100vh", backgroundImage: "url('/assets/spacebackground.png')", backgroundSize: "cover", backgroundRepeat: "no-repeat", position: "relative"}}>
+      <div
+        className="support-square"
+        style={{
+          width: '100%',
+          height: '100vh',
+          backgroundImage: "url('/assets/spacebackground.png')",
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+          position: 'relative',
+          display: 'flex', // Added flexbox
+          alignItems: 'center', // Vertically center the content
+        }}
+      >
+        <div
+          className="support-square"
+          style={{
+            width: '500px',
+            height: '450px',
+            backgroundImage: "url('/assets/spaceman.png')",
+            position: 'relative', // Changed to relative
+            flex: '0 0 40%', // Take up 50% of the parent container's width
+          }}
+        ></div>
+        <div
+          style={{color: 'white',
+            flex: '0 0 40%', // Take up 50% of the parent container's width
+          }}
+        >
+          <h1>Explore Our Stars</h1>
         </div>
       </div>
     </div>
-  )
+  );
 }
